4d1d7aa6e8dfc5ccc6c9b2a1ea0fe626cba6ccb5
Bonmyeong Koo - vn56eqd <Bonmyeong.Koo@walmart.com>
2025-06-09 16:45:34 -0400

    Initial Commit

---
+++ ShopSwift/ShopSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopSwiftApp.swift
+//  ShopSwift
+//
+//  Created by Bonmyeong Koo - Vendor on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
